c863b9063cd0468d20d736d0aad86685cb58ee72
Juan Londono <Juan Londono@JuanLondono>
2018-04-08 22:43:49 -0500

    Se crearon las variables numero1, numero2, suma y se imprime el resultado de la suma

---
+++ src/trabajo/Trabajo.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trabajo;
+
+/**
+ *
+ * @author Juan Londono
+ */
+public class Trabajo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        int numero1 = 2;
+        int numero2 = 3;
+        int suma = numero1 + numero2;
+        System.out.println("La suma es: " + suma);
+        
+    }
+    
+}
